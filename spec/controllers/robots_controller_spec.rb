ca780b1d6392d0ccdbfaca4c0f1b4414d8c89da6
Josh Mills <josh@trueheart78.com>
2016-08-05 14:54:20 -0400

    Deprecation removal

--- spec/controllers/robots_controller_spec.rb
+++ spec/controllers/robots_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe RobotsController, type: :controller do
-  describe 'GET #index' do
+  describe 'GET #index, format: txt' do
     it 'returns http success' do
       get :index, format: :txt
       expect(response).to have_http_status(:success)
