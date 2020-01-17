b39254e1d309d691e40b11e68d244fade9e8ab44
Josh Mills <josh@trueheart78.com>
2020-01-17 15:39:59 -0500

    deprecation fix

--- spec/controllers/robots_controller_spec.rb
+++ spec/controllers/robots_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe RobotsController, type: :controller do
   describe 'GET #index, format: txt' do
     it 'returns http success' do
       get :index, format: :txt
-      expect(response).to have_http_status(:success)
+      expect(response).to be_successful
     end
   end
 end
