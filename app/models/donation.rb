6a30c5ad106ab505cb22d511a7f44e03e069e44c
Josh Mills <josh@trueheart78.com>
2020-01-20 11:40:28 -0500

    More donation methods

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 class Donation < ApplicationRecord
+  def donation?
+    selection == 'donation'
+  end
+
+  def signup?
+    !donation?
+  end
+
+  def type
+    return 'donation' if selection == 'donation'
+
+    'signup'
+  end
+
   def amount
     amount_in_cents / 100.0
   end
