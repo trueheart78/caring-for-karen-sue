4f5a745c0c46339dd065c2cbc14075679f78510f
Josh Mills <josh@trueheart78.com>
2020-01-19 19:27:27 -0500

    Sponsors page stubbed out

---
+++ app/controllers/sponsors_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class SponsorsController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sponsors/index'
   get 'about', to: 'about#index'
+  get 'sponsors', to: 'sponsors#index'
   get 'robots.:format', to: 'robots#index'
 
   root 'welcome#index'
