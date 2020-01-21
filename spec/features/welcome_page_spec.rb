fc1ed09dc87e76eeb1684a6ca4abef00a7e6aec0
Josh Mills <josh@trueheart78.com>
2020-01-21 11:35:23 -0500

    Welcome page specs updated

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -7,19 +7,39 @@ RSpec.describe 'Welcome Page', type: :feature do
     visit root_path
   end
 
-  it 'Provides a link to the Home page' do
-    expect(page.find_link('Home')).to be_visible
+  context 'with navigation links' do
+    it 'Provides a link to the Home page' do
+      expect(page.find_link('Home')).to be_visible
+    end
+
+    it 'Provides a link to the Sponsor page' do
+      expect(page.find_link('Sponsor')).to be_visible
+    end
+
+    it 'Provides a link to the About page' do
+      expect(page.find_link('About')).to be_visible
+    end
+
+    it 'Provides three buttons to Sign Up' do
+      expect(page.all('li a', text: 'Sign Up').count).to eq(3)
+    end
+
+    it 'Provides a link to the Applewood Golf Course' do
+      expect(page.find_link('Applewood Golf Course')).to be_visible
+    end
+
+    it 'Provides a link to the Google Maps site' do
+      expect(page.find_link('Google Maps')).to be_visible
+    end
   end
 
-  it 'Provides a link to the Sponsor page' do
-    expect(page.find_link('Sponsor')).to be_visible
-  end
+  context 'with event details' do
+    it 'Provides the event date' do
+      expect(page.all('h3', text: 'Saturday, September 14th, 2019').count).to eq 1
+    end
 
-  it 'Provides a link to the About page' do
-    expect(page.find_link('About')).to be_visible
-  end
-
-  it 'Provides three buttons to Sign Up' do
-    expect(page.all('li a', text: 'Sign Up').count).to eq(3)
+    it 'Provides the event time' do
+      expect(page.all('p', text: '7:30am Shotgun Start').count).to eq 1
+    end
   end
 end
