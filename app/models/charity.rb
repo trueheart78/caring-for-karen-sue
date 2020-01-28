029053bc9c1beed637b1a785c940d7ddc8f93578
Josh Mills <josh@trueheart78.com>
2020-01-28 13:10:50 -0500

    Charity model added. About page now references Charity model.

---
+++ app/models/charity.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Charity
+  class << self
+    def name
+      'Shiloh House'
+    end
+
+    def url
+      'https://www.shilohhouse.org'
+    end
+
+    def logo
+      'shiloh-house.png'
+    end
+
+    def copy
+      <<~HTML.strip
+        The Shiloh House is a 501 (C) (3) that offers both residential and non-residential treatment for the treatment of male and female youth. They are greatly involved in foster care and they also give back and help the community. Visit them online at <a href='#{url}' target='_blank'>shilohhouse.org</a>
+      HTML
+    end
+  end
+end
