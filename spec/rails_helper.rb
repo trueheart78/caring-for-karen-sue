e56d2ff0eb4f612c01c8d8db9cf1c17bd0843f19
Josh Mills <josh@trueheart78.com>
2020-01-29 15:44:42 -0500

    Support for "testing" env

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require "support/simplecov"
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort("The Rails environment is not running in test mode!") unless Rails.env.test?
 require 'spec_helper'
 require 'rspec/rails'
 
