eb72264bc9bf6f1c3ef1a94e74f728d00413ee27
Josh Mills <josh@trueheart78.com>
2020-01-29 16:49:00 -0500

    Google analytics further setup and layed out and tested

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
   def current_charity
     Charity::RightStep
   end
+
+  def google_analytics?
+    google_analytics_key.present? && google_analytics_site.present?
+  end
+
+  def google_analytics_key
+    ENV['GOOGLE_ANALYTICS_KEY']
+  end
+
+  def google_analytics_site
+    ENV['GOOGLE_ANALYTICS_SITE']
+  end
 end
