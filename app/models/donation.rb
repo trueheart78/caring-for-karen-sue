e06defc90aa61a2a9fc6fcbc73a1eeb0975cf003
Josh Mills <josh@trueheart78.com>
2020-01-30 10:43:47 -0500

    Validations and initial specs for them

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Donation < ApplicationRecord
+  validates :name, length: { minimum: 2, maximum: 30 }
+  # https://api.rubyonrails.org/v5.2/classes/ActiveModel/Validations/ClassMethods.html#method-i-validates
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :selection, inclusion: { in: %w[donation registration lunch hole_sponser],
+                                     message: 'Invalid selection' }
+  validates :payment_type, inclusion: { in: %w[check paypal] }
+  validates :amount, numericality: { only_integer: true,
+                                     greater_than_or_equal_to: 1,
+                                     message: 'Amount must be a positive number' }
+
   def donation?
     selection == 'donation'
   end
--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -2,7 +2,36 @@
 
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe Donation, type: :model do
+  describe 'name validation' do
+    subject(:donation) { build :donation, name: name }
+
+    context 'when just right' do
+      let(:name) { 'test' }
+
+      it 'does not generate an error' do
+        expect(donation).to be_valid
+      end
+    end
+
+    context 'when too short' do
+      let(:name) { 't' }
+
+      it 'generates an error' do
+        expect(donation).not_to be_valid
+      end
+    end
+
+    context 'when too short' do
+      let(:name) { 't' * 31 }
+
+      it 'generates an error' do
+        expect(donation).not_to be_valid
+      end
+    end
+  end
+
   describe '#donation?' do
     subject(:donation) { create :donation, selection: selection }
 
@@ -43,3 +72,4 @@ RSpec.describe Donation, type: :model do
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
