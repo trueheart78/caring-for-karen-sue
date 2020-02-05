d98df31a1be8ca2cd887e45e21e5465db1a7bb4b
Josh Mills <josh@trueheart78.com>
2020-02-05 14:23:19 -0500

    Minor cleanup

--- app/helpers/donations/registrations_helper.rb
+++ app/helpers/donations/registrations_helper.rb
@@ -2,6 +2,8 @@
 
 module Donations::RegistrationsHelper
   def registration_options
-    Registration.all.map { |k, v| ["#{v} for $#{number_with_delimiter(k, delimiter: ',')} ", k] }
+    Registration.all.map do |k, v|
+      ["#{v} for $#{number_with_delimiter(k, delimiter: ',')} ", k]
+    end
   end
 end
