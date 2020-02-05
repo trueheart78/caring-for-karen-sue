0e4289da61c094cbb9aa739674f4b792a1f85432
Josh Mills <josh@trueheart78.com>
2020-02-05 11:17:36 -0500

    Basic forms for each model idea

--- app/controllers/donations/lunches_controller.rb
+++ app/controllers/donations/lunches_controller.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
 class Donations::LunchesController < ApplicationController
-  def new; end
+  def new
+    @donation = Donation.new
+  end
 
-  def create; end
+  def create
+    @donation = Donation.new(donation_params)
+    @donation.selection = 'lunch'
+
+    if @donation.save
+      redirect_to root_path, notice: "Your #{donation.selection} has been noted!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:name, :email, :amount, :payment_type)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     resources :lunches, only: [:new, :create]
     resources :donations, only: [:new, :create]
   end
+  get 'donations/hole_sponsors', to: 'donations/hole_sponsors#new'
+  get 'donations/registrations', to: 'donations/registrations#new'
+  get 'donations/lunches', to: 'donations/lunches#new'
+  get 'donations/donations', to: 'donations/donations#new'
   get 'about', to: 'about#index'
   get 'sponsors', to: 'sponsors#index'
   get 'robots.:format', to: 'robots#index'
