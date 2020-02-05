9abc13e42bbb1f2a1781b6ea88ae5c2e2c730a5a
Josh Mills <josh@trueheart78.com>
2020-02-05 15:17:34 -0500

    errors for forms

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 class Donation < ApplicationRecord
-  validates :name, length: { minimum: 2, maximum: 30 }
+  validates :name, length: { minimum: 2, maximum: 50, message: 'Name is invalid' }
   # https://api.rubyonrails.org/v5.2/classes/ActiveModel/Validations/ClassMethods.html#method-i-validates
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create,
+                              message: 'Email address is invalid' }
   validates :selection, inclusion: { in: %w[donation registration lunch hole_sponsor],
-                                     message: 'Invalid selection' }
-  validates :payment_type, inclusion: { in: %w[check paypal] }
+                                     message: 'Selection invalid' }
   validates :amount, numericality: { only_integer: true,
                                      greater_than_or_equal_to: 1,
                                      message: 'Amount must be a positive number' }
+  validates :payment_type, inclusion: { in: %w[check paypal],
+                                        message: 'Payment type must be selected' }
 
   def donation?
     selection == 'donation'
