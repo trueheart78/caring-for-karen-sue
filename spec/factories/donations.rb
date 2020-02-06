10833cfa1c74981534eca4feda084fa3731c4735
Josh Mills <josh@trueheart78.com>
2020-02-06 09:09:35 -0500

    Donation factory updated

--- spec/factories/donations.rb
+++ spec/factories/donations.rb
@@ -2,21 +2,24 @@
 
 FactoryBot.define do
   factory :donation do
-    name  { SecureRandom.hex(10) }
-    email { "#{name}@#{SecureRandom.hex(5)}.com" }
-    selection { 'donation' }
+    name         { SecureRandom.hex(10) }
+    email        { "#{name}@#{SecureRandom.hex(5)}.com" }
+    selection    { 'donation' }
     payment_type { 'check' }
-    amount { 100 }
+    amount       { 100 }
 
     factory :registration do
+      amount    { 500 }
       selection { 'registration' }
     end
 
     factory :lunch do
+      amount    { 150 }
       selection { 'lunch' }
     end
 
     factory :hole_sponsor do
+      amount    { 1_000 }
       selection { 'hole_sponsor' }
     end
   end
