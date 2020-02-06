8cad1d71feb079cf9ca5001327f2375a7a8dc21d
Josh Mills <josh@trueheart78.com>
2020-02-06 11:34:44 -0500

    Specs for success view

--- spec/views/successes/index.html.erb_spec.rb
+++ spec/views/successes/index.html.erb_spec.rb
@@ -3,5 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe 'successes/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'with a notification' do
+    let(:notification) { SecureRandom.hex 20 }
+
+    it 'displays the notification' do
+      flash[:notice] = notification
+      render
+      expect(rendered).to match notification
+    end
+
+    it 'informs of an outbound email' do
+      render
+      expect(rendered).to match 'You should receive an email from us shortly.'
+    end
+  end
+
+  describe 'without a notification' do
+    it 'informs of an outbound email' do
+      render
+      expect(rendered).to match 'You should receive an email from us shortly.'
+    end
+  end
 end
