b3c191508a0bb7f4ee8366ac45bf24b4c1e258b5
Josh Mills <josh@trueheart78.com>
2020-02-06 10:29:52 -0500

    Deprecation fixes

--- spec/controllers/donations/lunches_controller_spec.rb
+++ spec/controllers/donations/lunches_controller_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe Donations::LunchesController, type: :controller do
   describe 'GET #new' do
     it 'returns http success' do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to be_successful
     end
   end
 
   describe 'GET #create' do
     it 'returns http success' do
       get :create
-      expect(response).to have_http_status(:success)
+      expect(response).to be_successful
     end
   end
 end
