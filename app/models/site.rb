90f24f6fbae98e2fa25460643412e65e7551d971
Josh Mills <josh@trueheart78.com>
2021-06-03 16:54:20 -0400

    New Site model and Open Graph tags.
    * Site specs added.
    * Site replacements for the proper locations in views.
    * Removed empty favicon.ico file.
    * New public/images/gloves.png file.
    * New Open Graph Tags partial.

---
+++ app/models/site.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Site
+  class << self
+    def title
+      'Caring for Karen Sue'
+    end
+
+    def website
+      'CaringForKarenSue.com'
+    end
+
+    def url
+      'https://www.caringforkarensue.com'
+    end
+
+    def facebook_url
+      'http://facebook.com/caringforkarensue'
+    end
+
+    def description(iteration:)
+      [
+        "Come register for the #{iteration} Annual Golf Classic, ",
+        'a four man best ball-scramble type tournament.'
+      ].join
+    end
+
+    def image_url
+      File.join url, image
+    end
+
+    def image
+      'images/gloves.png'
+    end
+  end
+end
