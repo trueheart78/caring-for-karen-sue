9a81de7543556b58155586ee805850488a4ea1ff
Josh Mills <josh@trueheart78.com>
2021-06-07 11:38:20 -0400

    New response header: Permissions-Policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   rescue_from ActionController::RoutingError, with: :error_render_method
+  after_action :custom_response_headers
+
+  def custom_response_headers
+    response.set_header 'Permissions-Policy', "geolocation=(self \"#{Site.url}\"), microphone=()"
+  end
 
   def route_not_found
     render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
