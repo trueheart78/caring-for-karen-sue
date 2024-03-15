88e0fd80ea9c03e2304870a4071dc49a92f7fe3e
Josh Mills <josh@trueheart78.com>
2024-03-15 14:12:17 -0400

    db/schema.rb updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_181331) do
+ActiveRecord::Schema[6.1].define(version: 2020_06_10_181331) do
 
   create_table "donations", force: :cascade do |t|
     t.string "email", null: false
