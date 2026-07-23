362ca92b24c292c6bbb4f7721b45d75611d1083a
Josh Mills <josh@trueheart78.com>
2026-07-22 20:17:16 -0400

    add support for the two extra rb files in the manifest.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,5 +3,12 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
-# Add additional assets to the asset load path (deprecated by manifest.js)
-# Rails.application.config.assets.precompile += %w[]
+# Register MIME types for favicon ERB templates
+Rails.application.config.assets.configure do |env|
+  env.register_mime_type("application/manifest+json",
+    extensions: [".webmanifest", ".webmanifest.erb"])
+  env.register_preprocessor("application/manifest+json", Sprockets::ERBProcessor)
+
+  env.register_mime_type("application/xml", extensions: [".xml", ".xml.erb"])
+  env.register_preprocessor("application/xml", Sprockets::ERBProcessor)
+end
